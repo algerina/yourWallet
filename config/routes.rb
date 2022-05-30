373fa01da7c92d07319af826dcdb722ec4f5f32b
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-31 00:29:44 +0100

    Fix linters errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do  
-    get '/users/sign_out' => 'devise/sessions#destroy'     
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
   end
 
-
-  root to: "home#index"
+  root to: 'home#index'
 end
