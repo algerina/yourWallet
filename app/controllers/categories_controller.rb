d1683bb799d5e4a7e20bb97a6b74c64e001095cc
Amel Khiri <53347966+algerina@users.noreply.github.com>
2022-05-31 23:58:07 +0100

    Update categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,11 +3,7 @@ class CategoriesController < ApplicationController
 
   # GET /categories or /categories.json
   def index
-    @categories = Category.all
-  end
-
-  # GET /categories/1 or /categories/1.json
-  def show
+    @categories = current_user.categories
   end
 
   # GET /categories/new
@@ -15,37 +11,21 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
-  # GET /categories/1/edit
-  def edit
-  end
 
   # POST /categories or /categories.json
   def create
-    @category = Category.new(category_params)
+    @category = current_user.Category.new(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
+        redirect_to groups_path, notice: 'Category has been created!'
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
+        flash[:alert] = 'Something went wrong, Try again!'
+      render :new
       end
     end
   end
 
-  # PATCH/PUT /categories/1 or /categories/1.json
-  def update
-    respond_to do |format|
-      if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /categories/1 or /categories/1.json
   def destroy
--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,2 +1,3 @@
 class SplashController < ApplicationController
+  def index; end
 end
